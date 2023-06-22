da58b7d857660ac665d2b9490cc257fde4534e81
Dean Cahill <dpcahill98@gmail.com>
2023-06-22 11:34:02 -0400

    name change

--- metadata.py
+++ metadata.py
@@ -25,8 +25,8 @@ def appmetadata() -> AppMetadata:
         name="Chyron Recognition",
         description="This tool detects chyrons and generates time segments.",
         app_license="MIT",  
-        identifier="chyron-recognition",
-        url="https://github.com/clamsproject/app-chyronrecognition",
+        identifier="chyron-text-recognition",
+        url="https://github.com/clamsproject/app-chyron-text-recognition",
     )
 
     # I/O Spec
